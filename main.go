74f0c1d044129876dc943d0a610c8afe9dcbde0c
Andrew Snodgrass <pointlander@gmail.com>
2025-05-05 16:43:06 -0500

    Improved feedback

--- main.go
+++ main.go
@@ -23,22 +23,38 @@ func Integrate(depth int, expression string) *Node {
 	}
 	a := calc.Tree()
 	seed := 1
+	values := []float64{.1, -.1, 1, -1, 2, -2, 3, -3}
+	type Element struct {
+		Index int
+		Value float64
+	}
 	for {
 		rng := rand.New(rand.NewSource(int64(seed)))
 		s := NewSource()
 		last := ""
-		points := 1
 		for {
 			r := s.Samples(depth, rng)
+			d := make([][]Element, len(values))
 			for j, v := range r {
 				b := v.Root.Derivative()
-				for k := 0; k < points; k++ {
-					z := float64(k + 1)
+				dd := make([]float64, 0, 8)
+				for k := range values {
+					z := values[k]
 					aa := a.Calculate(z)
 					bb := b.Calculate(z)
 					diff := aa - bb
+					dd = append(dd, diff*diff)
 					if math.IsInf(diff, 0) || math.IsNaN(diff) {
 						r[j].Fitness = math.Inf(1)
+						d[k] = append(d[k], Element{
+							Index: j,
+							Value: math.Inf(1),
+						})
+					} else {
+						d[k] = append(d[k], Element{
+							Index: j,
+							Value: math.Abs(diff),
+						})
 					}
 					if !(math.IsInf(r[j].Fitness, 0) || math.IsNaN(r[j].Fitness)) {
 						r[j].Fitness += diff * diff
@@ -48,65 +64,90 @@ func Integrate(depth int, expression string) *Node {
 			sort.Slice(r, func(i, j int) bool {
 				return r[i].Fitness < r[j].Fitness
 			})
+			if r[0].Fitness == 0 {
+				return r[0].Root
+			}
+
 			if last == r[0].Root.String() {
 				break
 			}
 			last = r[0].Root.String()
-			if r[0].Fitness == 0 {
-				if points > 2 {
-					return r[0].Root
-				}
-				points++
+
+			for k := range d {
+				sort.Slice(d[k], func(i, j int) bool {
+					return d[k][i].Value < d[k][j].Value
+				})
 			}
 			index := 0
-			for _, v := range r {
-				if math.IsInf(v.Fitness, 0) {
-					break
+		outer:
+			for i := range d[0] {
+				for k := range d {
+					if math.IsInf(d[k][i].Value, 0) {
+						break outer
+					}
 				}
 				index++
 			}
-			r = r[:index]
+			for k := range d {
+				d[k] = d[k][:index]
+			}
+
 			if index > 0 {
-				count, sum := 0.0, 0.0
-				for _, v := range r {
-					count++
-					sum += v.Fitness
+				min := len(d[0])
+				for k := range d {
+					count, sum := 0.0, 0.0
+					for _, v := range d[k] {
+						count++
+						sum += v.Value
+					}
+					average := sum / count
+					variance := 0.0
+					for _, v := range d[k] {
+						diff := average - v.Value
+						variance += diff * diff
+					}
+					variance /= float64(len(d[k]))
+					max, cut := 0.0, len(d[k])-1
+					for i := 1; i < len(d[k])-1; i++ {
+						avga, avgb := 0.0, 0.0
+						vara, varb := 0.0, 0.0
+						for j := 0; j < i; j++ {
+							avga += d[k][j].Value
+						}
+						avga /= float64(i)
+						for j := 0; j < i; j++ {
+							diff := d[k][j].Value - avga
+							vara += diff * diff
+						}
+						vara /= float64(i)
+						for j := i; j < len(d[k]); j++ {
+							avgb += d[k][j].Value
+						}
+						avgb /= float64(len(d[k]) - i)
+						for j := i; j < len(d[k]); j++ {
+							diff := d[k][j].Value - avgb
+							varb += diff * diff
+						}
+						varb /= float64(len(d[k]) - i)
+						reduction := variance - (vara + varb)
+						if reduction > max {
+							max, cut = reduction, i
+						}
+					}
+					if cut < min {
+						min = cut
+					}
 				}
-				average := sum / count
-				variance := 0.0
-				for _, v := range r {
-					diff := average - v.Fitness
-					variance += diff * diff
-				}
-				variance /= float64(len(r))
-				max, cut := 0.0, 0
-				for i := 1; i < len(r)-1; i++ {
-					avga, avgb := 0.0, 0.0
-					vara, varb := 0.0, 0.0
-					for j := 0; j < i; j++ {
-						avga += r[j].Fitness
-					}
-					avga /= float64(i)
-					for j := 0; j < i; j++ {
-						diff := r[j].Fitness - avga
-						vara += diff * diff
-					}
-					vara /= float64(i)
-					for j := i; j < len(r); j++ {
-						avgb += r[j].Fitness
-					}
-					avgb /= float64(len(r) - i)
-					for j := i; j < len(r); j++ {
-						diff := r[j].Fitness - avgb
-						varb += diff * diff
-					}
-					varb /= float64(len(r) - i)
-					reduction := variance - (vara + varb)
-					if reduction > max {
-						max, cut = reduction, i
+				common := make(map[int]int)
+				for k := range d {
+					for j := 0; j < len(d[k]); j++ {
+						common[d[k][j].Index]++
 					}
 				}
-				r = r[:cut]
+				sort.Slice(r, func(i, j int) bool {
+					return common[i] < common[j]
+				})
+				r = r[:min]
 			}
 			r.Statistics(s)
 		}
